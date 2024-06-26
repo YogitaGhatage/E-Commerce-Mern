b28a0b46369e9b91e5c82a214068badbb86522fa
YogitaGhatage <ghatageyogita2002@gmail.com>
2024-06-26 13:53:52 +0530

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { Container } from "react-bootstrap" 
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+
 
 const App = () => {
   return (
-    <p>
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+    <h1>
       WelCome To ProShop
-    </p>
+    </h1>
+    </Container>
+        <Outlet />
+    </main>
+    <Footer />
+    </>
   )
 }
 
---
+++ frontend/src/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+    _id : '1',
+    name: 'Airpods Wirelsee',
+    image : '/imagex/airpods.png',
+    description: "Bluetooth technology",
+    brand : 'Apple',
+    category : 'Electronics',
+    price : 149.99,
+    countInStock : 10,
+    rating : 4.5,
+    numReviews : 12,
+    },
+    {
+        _id : '1',
+        name: 'Airpods Wirelsee',
+        image : '/imagex/airpods.png',
+        description: "Bluetooth technology",
+        brand : 'Apple',
+        category : 'Electronics',
+        price : 149.99,
+        countInStock : 10,
+        rating : 4.5,
+        numReviews : 12,
+        },
+]
+
+export default products
