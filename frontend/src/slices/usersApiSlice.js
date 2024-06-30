37d915b1025501ff4428a969782681d5c5cecb21
YogitaGhatage <ghatageyogita2002@gmail.com>
2024-06-30 14:21:56 +0530

    Commit

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,29 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({ //Send request to backend and accept the cookie
+            query: (data) => ({
+                url: `${USERS_URL}/auth`,
+                method: "POST",
+                body: data,
+            }),
+        }),
+        register: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/register`,
+                method: "POST",
+                body: data,,
+            }),
+         }),//Send request to backend and accept the cookie
+        logout: builder.mutation({
+            query: () => ({
+                url: `${USERS_URL}/logout`,
+                method: "POST",//This is for the server
+            })
+        })
+    }),
+});
+
+export const { useLoginMutation, useLogoutMutation } = usersApiSlice;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  cartSliceReucer  from './slices/cartSlice';
 import { apiSlice } from "./slices/apiSlice";
+import  cartSliceReucer  from './slices/cartSlice';
+import authSliceReducer from "./slices/authSlice";
 
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         cart: cartSliceReucer,
+        auth: authSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
