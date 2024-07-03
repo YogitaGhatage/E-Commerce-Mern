7ec60fd5018444300deaec1202f91264b48dd743
YogitaGhatage <ghatageyogita2002@gmail.com>
2024-07-03 15:07:26 +0530

    Commit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -36,7 +36,7 @@ const HomeScreen = () => {
         <>
           <Meta />
           <h1>Latest Products</h1>
-          <Row>
+          <Row >
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
